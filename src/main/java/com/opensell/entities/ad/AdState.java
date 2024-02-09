adbd1256d34bb1880ef19fa0530fcc748bca7a72
AchrafGroiez <41470d0a@gmail.com>
2024-02-09 00:03:32 -0500

    Added Ad State

---
+++ src/main/java/com/opensell/entities/ad/AdState.java
@@ -0,0 +1,8 @@
+package com.opensell.entities.ad;
+
+public enum AdState {
+	PUBLIC,
+	PRIVATE,
+	LINK_ONLY,
+	SOLD
+}
--- src/main/java/com/opensell/entities/ad/AdType.java
+++ src/main/java/com/opensell/entities/ad/AdType.java
@@ -7,7 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-	@Entity @Data @AllArgsConstructor @NoArgsConstructor
+@Entity @Data @AllArgsConstructor @NoArgsConstructor
 public class AdType {
     @Id private int idAdType;
     
