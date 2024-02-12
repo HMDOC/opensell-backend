547f4abee65414633b41206503652dfdf2b15b45
AchrafGroiez <41470d0a@gmail.com>
2024-02-12 00:50:06 -0500

    Solved Import Error in IndexController.java

--- src/main/java/com/opensell/controller/IndexController.java
+++ src/main/java/com/opensell/controller/IndexController.java
@@ -1,13 +1,6 @@
 package com.opensell.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.opensell.entities.dto.AdView;
-import com.opensell.entities.test.Factor;
-import com.opensell.entities.test.Obj;
-import com.opensell.entities.test.Prod;
 
 @RestController
 public class IndexController {
