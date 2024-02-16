5124d85d2010e61c2cc45ef9bc6709763e1ae288
Messicat <112326931+JF195@users.noreply.github.com>
2024-02-15 22:10:58 -0500

    Basic Search functionality

--- src/main/java/com/opensell/repository/AdRepository.java
+++ src/main/java/com/opensell/repository/AdRepository.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.opensell.entities.Ad;
-import com.opensell.entities.ad.AdVisibility;
 import com.opensell.entities.dto.AdSearchPreview;
 
 @Repository
@@ -15,14 +15,20 @@ public interface AdRepository extends JpaRepository<Ad, Integer> {
 	@Query("SELECT a FROM Ad a WHERE a.link = ?1 AND a.isDeleted = false AND a.visibility != 1")
 	public Ad getAdByLink(String link);
 	
+	 // Unfinished. https://www.baeldung.com/spring-jpa-like-queries
+		@Query("SELECT a FROM Ad a "
+				+ "WHERE (a.isDeleted = false AND a.visibility != 1 AND (a.title LIKE %:search% OR a.description LIKE %:search%)) "
+				+ "ORDER BY a.addedDate DESC")
+		public List<Ad> getUnfilteredSearch(@Param("search") String searchName);
+	
 	/*
-	 * Unfinished
+	 // Unfinished. https://www.baeldung.com/spring-jpa-like-queries
 	@Query("SELECT new com.opensell.entities.dto.AdSearchPreview(a.title, a.price, a.shape, a.isSold, a.link, '') FROM Ad a "
-			+ "WHERE (a.isDeleted = false AND a.visibility != 1 AND (a.title LIKE ?1 OR a.description LIKE ?1)) "
+			+ "WHERE (a.isDeleted = false AND a.visibility != 1 AND (a.title LIKE %:search% OR a.description LIKE %:search%)) "
 			+ "ORDER BY a.addedDate DESC")
-	public List<AdSearchPreview> getUnfilteredSearch(String searchName);
-	*/
+	public List<AdSearchPreview> getUnfilteredSearch(@Param("search") String searchName);
 	 
+	 */
 	/*
 	 * Filters:
 	 * Prix 
