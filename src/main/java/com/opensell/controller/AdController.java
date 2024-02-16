c2dce4dd997736f4c71b218253b5877af66b1eb3
AchrafGroiez <41470d0a@gmail.com>
2024-02-16 14:33:35 -0500

    Resolved merge

--- src/main/java/com/opensell/controller/AdController.java
+++ src/main/java/com/opensell/controller/AdController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import com.opensell.entities.Ad;
 import com.opensell.entities.Customer;
 import com.opensell.entities.dto.AdBuyerView;
+import com.opensell.entities.dto.AdSearchPreview;
 import com.opensell.repository.AdRepository;
 
 @CrossOrigin(value = "http://localhost/")
---
+++ src/main/java/com/opensell/entities/dto/AdModifView.java
@@ -0,0 +1,21 @@
+package com.opensell.entities.dto;
+
+import java.util.Set;
+
+/**
+ * Record that contain the essential data when a user is trying to UPDATE a Ad.
+ * 
+ * @author Achraf
+ */
+public record AdModifView(String title,
+						  double price,
+						  int shape,
+						  boolean isSold,
+						  int visibility,
+						  String description,
+						  String reference,
+						  String address,
+						  String link,
+						  String adTypeName,
+						  Set<String> adTagsName,
+						  String adImagesPath) {}
