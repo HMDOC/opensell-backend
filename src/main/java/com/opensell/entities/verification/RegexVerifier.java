69521cea8692715d4ae9c69929b7ed11eee0e6a4
olvrm <oliviermansuy7@gmail.com>
2024-03-15 21:17:50 -0400

    Changed one regex

--- src/main/java/com/opensell/entities/verification/RegexVerifier.java
+++ src/main/java/com/opensell/entities/verification/RegexVerifier.java
@@ -10,7 +10,7 @@ public enum RegexVerifier {
 
     EMAIL("^(?=.{1,64}@)[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9]+)*@[a-z]+(\\.[a-zA-Z]+)+[a-zA-Z]$"),
     FIRST_LAST_NAME("^([a-zA-Z]){1,15}((-|\\s)[a-zA-Z]{1,15}){0,2}"),
-    PHONE_NUMBER("^((\\(\\d{3}\\))|(\\d{3}))((\\s\\d{3})|(-\\d{3}))(-(\\d{4})|(\\s\\d{4}))"),
+    PHONE_NUMBER("^\\d{3}-\\d{3}-\\d{4}$"),
     USERNAME("^([a-zA-Z0-9_]){1,30}$"),
     PWD("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{8,}"),
     URL("(https?:\\/\\/)?(www\\.)?(?=[a-zA-Z0-9]{1,256}\\.)[a-zA-Z0-9]+(\\.[a-z]{2,5}){1,3}(\\/[a-zA-Z0-9-\\._~:\\/?#\\[\\]@!$&'\\(\\)\\*\\+,;=]*)?"),
@@ -24,9 +24,5 @@ public enum RegexVerifier {
 
     public boolean verify(String attribute) {
         return Pattern.compile(this.value).matcher(attribute).matches();
-    }
-
-    public static ResponseEntity<Integer> wrongFormatResponse() {
-        return new ResponseEntity<>(CustomerModificationCode.WRONG_FORMAT, HttpStatus.BAD_REQUEST);
     }
 }
