7c454e510188b9ea439551bee15a8ce35e5df8f9
AchrafGroiez <41470d0a@gmail.com>
2024-07-26 12:43:32 -0400

    Moved PasswordEncoder bean from BackendApplication.java to SecurityConfig.java.

--- src/main/java/com/opensell/BackendApplication.java
+++ src/main/java/com/opensell/BackendApplication.java
@@ -4,21 +4,12 @@ import com.opensell.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableConfigurationProperties(value = {AppConfig.class})
 @EnableScheduling
 @SpringBootApplication
 public class BackendApplication {
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
