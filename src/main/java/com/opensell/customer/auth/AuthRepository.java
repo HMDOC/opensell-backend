fafb833f4a8cac1a2c75e945882754891926b898
AchrafGroiez <41470d0a@gmail.com>
2024-08-04 21:19:56 -0400

    Removed old SQL comment from code that was successfuly converted to mongodb.

--- src/main/java/com/opensell/customer/auth/AuthRepository.java
+++ src/main/java/com/opensell/customer/auth/AuthRepository.java
@@ -11,20 +11,15 @@ import com.opensell.model.Customer;
 
 @Repository
 public interface AuthRepository extends MongoRepository<Customer, String> {
-    //login
-    //@Query("SELECT * FROM customer c WHERE c.email = ?1 OR c.username = ?1 AND c.is_deleted = false AND c.is_activated = true")
     @Query("{ isDeleted: false, isActivated: true, $or: [{ email: ?0 }, { username: ?0 }] }")
     Customer getUser(String usernameOrEmail);
 
-    //@Query("SELECT count(c.email) FROM customer c WHERE c.email = ?1 LIMIT 1")
     @ExistsQuery("{email: ?0}")
     boolean existsByEmail(String email);
 
-    //@Query("SELECT count(c.username) FROM customer c WHERE c.username = ?1 LIMIT 1")
     @ExistsQuery("{username: ?0}")
     boolean existsByUsername(String username);
 
-    //@Query(value = "UPDATE customer c SET c.is_activated = 1 where c.email = ?1 LIMIT 1")
     @Query("{email: ?0}")
     @Update("{$set: {isActivated: true}}")
     int activateAccount(String email);
--- src/main/java/com/opensell/customer/setting/SettingRepository.java
+++ src/main/java/com/opensell/customer/setting/SettingRepository.java
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.opensell.model.Customer;
 
 /**
- * @author Olivier Mansuy
- * @Note Methods given by the repository are not used so that we can have all the modification methods
- * at the same place
+ * @author Olivier, Achraf
  */
 @Repository
 public interface SettingRepository extends MongoRepository<Customer, String> {
@@ -18,11 +16,9 @@ public interface SettingRepository extends MongoRepository<Customer, String> {
     @Update("{$set: {iconPath: ?0}}")
     int updateCustomerIconPath(String iconPath, String customerId);
 
-    //@Query(value = "SELECT EXISTS(SELECT * FROM customer c WHERE c.id != ?1 AND c.email = ?2)", nativeQuery = true)
     @ExistsQuery("{id: {$ne: ?0}, email: ?1}")
     boolean isEmailExist(String id, String email);
 
-    //@Query(value = "SELECT EXISTS(SELECT * FROM customer c WHERE c.id != ?1 AND c.username = ?2)", nativeQuery = true)
     @ExistsQuery("{id: {$ne: ?0}, username: ?1}")
     boolean isUsernameExist(String id, String username);
 }
