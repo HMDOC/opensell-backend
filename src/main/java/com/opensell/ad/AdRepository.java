4d0b1755564b05db60fc6d5f4c70802eda03cdcf
Messicat <112326931+JF195@users.noreply.github.com>
2024-08-16 15:38:47 -0400

    removed unused imports

--- src/main/java/com/opensell/ad/AdRepository.java
+++ src/main/java/com/opensell/ad/AdRepository.java
@@ -5,12 +5,10 @@ import com.opensell.model.Ad;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
