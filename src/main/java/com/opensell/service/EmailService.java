c2b6d19aa409e709104e131a9d103b0f1c27b306
AchrafGroiez <41470d0a@gmail.com>
2024-10-12 15:31:55 -0400

    Added the SMTP server in the docker compose.

--- src/main/java/com/opensell/service/EmailService.java
+++ src/main/java/com/opensell/service/EmailService.java
@@ -1,14 +1,9 @@
 package com.opensell.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
-
 import com.opensell.config.AppConfig;
-
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -20,7 +15,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class EmailService {
     private final AppConfig appConfig;
-    private final JavaMailSender javaMailSender = new JavaMailSenderImpl();
+    private final JavaMailSender javaMailSender;
 
     public boolean sendEmail(String email, String subject, String text) {
         try {
