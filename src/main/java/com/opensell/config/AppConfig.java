3563b6a7d580dc712b8e5f3a1b58d1f6ee6ca34b
AchrafGroiez <41470d0a@gmail.com>
2024-10-16 07:14:34 -0400

    Removed unused application property named serverUrl, moved default value to dev profile instead of combining them in default profile and updated README to current application situation.

--- src/main/java/com/opensell/config/AppConfig.java
+++ src/main/java/com/opensell/config/AppConfig.java
@@ -4,6 +4,6 @@ import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "app")
-public record AppConfig(List<String> allowedUrls, String serverUrl, String uploadPath, String supportEmail) {
+public record AppConfig(List<String> allowedUrls, String uploadPath, String supportEmail) {
 
 }
